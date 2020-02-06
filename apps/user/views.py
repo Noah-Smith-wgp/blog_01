65815fc2bf77d18e10512a4f1ea5ad69b7c536f6
Noah-Smith-wgp <18211672297@163.com>
2020-02-06 11:56:50 +0800

    简单注册逻辑

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.db import DatabaseError
+from django.http import HttpResponseBadRequest, HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.views import View
 
 # Create your views here.
+from apps.user.models import User
 
 
 class RegisterView(View):
@@ -9,3 +13,24 @@ class RegisterView(View):
     def get(self, request):
 
         return render(request, 'register.html')
+
+    def post(self, request):
+        # 接收参数
+        mobile = request.POST.get('mobile')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        smscode = request.POST.get('sms_code')
+
+        # 判断参数是否齐全
+        if not all([mobile, password, password2, smscode]):
+            return HttpResponseBadRequest('缺少必传参数')
+
+        # 保存注册数据
+        try:
+            user = User.objects.create_user(username=mobile, mobile=mobile, password=password)
+        except DatabaseError:
+            return HttpResponseBadRequest('注册失败')
+
+        # return redirect(reverse())
+        # return redirect('/')
+        return HttpResponse('注册成功，重定向到首页')
